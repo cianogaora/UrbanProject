a00c00b028d21913d124b9984f39b5bd69e293ba
ogaorac <ogaorac@tcd.ie>
2022-11-23 18:46:14 +0000

    Activities

---
+++ app/src/main/java/com/example/urbancomputing/DailyActivity.kt
@@ -0,0 +1,15 @@
+package com.example.urbancomputing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class DailyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily)
+    }
+}
